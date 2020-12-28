a2ecf44539b654471919911f5b5492068dbb125c
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2020-12-28 18:52:47 +0200

    NM-62 and NM-63 redesign

--- src/components/Fotograf/CereriAcceptDecline.vue
+++ src/components/Fotograf/CereriAcceptDecline.vue
@@ -1,39 +1,53 @@
 <template>
   <v-container fluid grid-list-xl>
     <v-layout align-center justify-space-around row>
-      <v-data-table
-        :headers="headers"
-        :items="getBookingsForPhoto"
-        item-key="name"
-        class="elevation-1">
-        <template slot="headerCell" slot-scope="props">
-          <v-tooltip bottom>
-            <span slot="activator">
-              {{ props.header.text }}
-            </span>
-            <span>
-              {{ props.header.text }}
-            </span>
-          </v-tooltip>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                Client
+              </th>
+              <th class="text-left">
+                Period
+              </th>
+              <th class="text-left">
+                Status
+              </th>
+              <th class="text-left"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in getBookingsForPhoto" :key="item.prenume">
+              <td class="text-xs-left">{{ item.idClient }}</td>
+              <td class="text-xs-left">{{ item.dataStart }}</td>
+              <td class="text-xs-left">{{ item.dataEnd }}</td>
+              <td
+                v-bind:class="{
+                  'red--text': item.status === 'pending',
+                  'green--text': item.status === 'approved',
+                  'orange--text': item.status === 'declined',
+                }"
+              >
+                {{ item.status }}
+              </td>
+              <td v-if="item.status === 'pending'">
+                <v-btn text small color="green" @click="aprove(item.idCerere)">
+                  Aproba cerere
+                </v-btn>
+                <v-btn
+                  text
+                  small
+                  color="orange"
+                  @click="decline(item.idCerere)"
+                >
+                  Respinge cerere
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
         </template>
-        <template slot="items" slot-scope="props">
-          <td class="text-xs-left">{{ props.item.idClient }}</td>
-          <td class="text-xs-left">{{ props.item.dataStart }}</td>
-          <td class="text-xs-left">{{ props.item.dataEnd }}</td>
-          <td v-bind:class="{'red--text': props.item.status === 'pending', 
-                             'green--text': props.item.status === 'approved', 
-                             'orange--text': props.item.status === 'declined'
-                            }">{{ props.item.status}}</td>
-          <td v-if="props.item.status === 'pending'">
-            <v-btn text small color="green" @click="aprove(props.item.idCerere)">
-              Aproba cerere
-            </v-btn>
-            <v-btn text small color="orange" @click="decline(props.item.idCerere)">
-              Respinge cerere
-            </v-btn>
-          </td>
-        </template>
-      </v-data-table>
+      </v-simple-table>
     </v-layout>
   </v-container>
 </template>
@@ -41,80 +55,94 @@
 <script>
 import firebase from "firebase";
 export default {
-    data () {
+  data() {
     return {
       send: false,
       status: [],
       statuses: [
         {
-          text: 'Aprobate', value: 'approved'
+          text: "Aprobate",
+          value: "approved",
         },
         {
-          text: 'Anulate', value: 'declined'
+          text: "Anulate",
+          value: "declined",
         },
         {
-          text: 'In asteptare', value: 'pending'
-        }
+          text: "In asteptare",
+          value: "pending",
+        },
       ],
       items: [],
       id: null,
       headers: [
         {
-          text: 'Client', align: 'left', value: 'user'
+          text: "Client",
+          align: "left",
+          value: "user",
         },
         {
-          text: 'Perioada solicitata', value: 'university'
+          text: "Perioada solicitata",
+          value: "university",
         },
         {
-          text: 'status', value: 'statuses'
+          text: "status",
+          value: "statuses",
         },
         {
-          text: '', value: 'actions'
-        }
+          text: "",
+          value: "actions",
+        },
       ],
       indexes: null,
-      data: []
-    }
+      data: [],
+    };
   },
   computed: {
     bookingDetails() {
-      return this.$store.getters.bookingsDetails
+      return this.$store.getters.bookingsDetails;
     },
     user() {
-      return this.$store.getters.user
+      return this.$store.getters.user;
     },
     getBookingsForPhoto() {
-      let detalii = []
-      this.bookingDetails.forEach(element => {
+      let detalii = [];
+      this.bookingDetails.forEach((element) => {
         if (element.idFotograf === this.user.id) {
-          detalii.push(element)
+          detalii.push(element);
         }
       });
-      return detalii
-    }
+      return detalii;
+    },
   },
   methods: {
     getPhotographersData() {
-      let x = []
-      this.$store.getters.photographersDetails.filter(item => { 
-        this.getBookingsForPhoto.forEach(photographer => {
+      let x = [];
+      this.$store.getters.photographersDetails.filter((item) => {
+        this.getBookingsForPhoto.forEach((photographer) => {
           if (photographer.idFotograf === item.idFotograf) {
-            x.push(item)
+            x.push(item);
           }
-        })
-      })
-      this.data = x
+        });
+      });
+      this.data = x;
     },
-    aprove (value) {
-      firebase.database().ref('rezervari/'+ value).update({
-        status: 'approved'
-      })
+    aprove(value) {
+      firebase
+        .database()
+        .ref("rezervari/" + value)
+        .update({
+          status: "approved",
+        });
     },
-    decline (value) {
-      firebase.database().ref('rezervari/'+ value).update({
-        status: 'declined'
-      })
-    }
-  }
-}
+    decline(value) {
+      firebase
+        .database()
+        .ref("rezervari/" + value)
+        .update({
+          status: "declined",
+        });
+    },
+  },
+};
 </script>
